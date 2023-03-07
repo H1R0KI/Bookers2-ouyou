2b16b4a0c50057dc390a617f237d25cde489cc40
Hiroki komatsu <122168203+H1R0KI@users.noreply.github.com>
2023-03-07 14:23:49 +0000

    [add]グループDM機能

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,17 @@ Rails.application.configure do
   config.hosts << "34cf2f293122477bbe3716415b326493.vfs.cloud9.us-east-1.amazonaws.com"
 
   config.active_job.queue_adapter = :inline
+
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      port:                 587,
+      address:              'smtp.gmail.com',
+      domain:               'gmail.com',
+      user_name:            'hiroki.komatsu0610@gmail.com',
+      password:             'lltvutwxvsxnwfld',
+      authentication:       'login',
+      enable_starttls_auto: true
+    }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,9 @@ Rails.application.routes.draw do
 
   resources :groups, only: [:index, :show, :edit, :create, :update, :new, :show]
   resources :groups do
-    get "join" => "groups#join"
+    get "join" =>"groups#join"
+    get "new/mail" => "groups#new_mail"
+    get "send/mail" => "groups#send_mail"
   end
 
   get "search" => "searches#search"
