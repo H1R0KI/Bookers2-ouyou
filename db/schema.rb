ab3c4237fb2612a367a97002a0237e7b9cbcbcb8
H1R0KI <122168203+H1R0KI@users.noreply.github.com>
2023-03-11 14:24:46 +0000

    [Start]タグ追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_05_135124) do
+ActiveRecord::Schema.define(version: 2023_03_11_142359) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2023_03_05_135124) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "tag"
   end
 
   create_table "chats", force: :cascade do |t|
